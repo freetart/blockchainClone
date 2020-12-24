6920ce0564f5df25f55dd8732bda6d3c80b233e7
Chris M <habibdev96@gmail.com>
2020-12-24 03:57:16 -0700

    created & styled unique hamburger menu

---
+++ js/app.js
@@ -0,0 +1,4 @@
+const menuIcon = document.querySelector(".hamburger-menu");
+const menuNavbar = document.querySelector(".menu-navbar");
+
+menuIcon.addEventListener("click", () => menuNavbar.classList.toggle("change"));
